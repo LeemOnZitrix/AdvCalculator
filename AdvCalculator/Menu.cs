196c74cb96c0b882b07c4039fdbf7b3e20e8966e
LeemOnZitrix <do.pawlus@gmail.com>
2022-05-16 11:31:42 +0200

    pierwsza wersja

---
+++ AdvCalculator/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvCalculator
+{
+    public class Menu
+    {
+        public void MainMenu()
+        {
+            Console.WriteLine("Witaj w kalkulatorze, wybierz operację");
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
+            Console.WriteLine("4. Dzielenie");
+            Console.WriteLine("5. Operacje specjalne");          
+        }
+    }
+}
--- AdvCalculator/Program.cs
+++ AdvCalculator/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdvCalculator;
+
+int number1;
+int number2;
+Menu Menu = new Menu();
+
+Menu.MainMenu();
+int operation = Convert.ToInt32(Console.ReadLine());
+if (operation == 1)
+{
+    Console.WriteLine("Wybierz na klawiaturze pierwszą liczbę");
+    number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wybierz na klawiaturze drugą liczbę");
+    number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wynik to");
+    Console.WriteLine(number1 + number2);
+}
+else if (operation == 2)
+{
+    Console.WriteLine("Wybierz na klawiaturze pierwszą liczbę");
+    number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wybierz na klawiaturze drugą liczbę");
+    number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wynik to");
+    Console.WriteLine(number1 - number2);
+}
+else if (operation == 3)
+{
+    Console.WriteLine("Wybierz na klawiaturze pierwszą liczbę");
+    number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wybierz na klawiaturze drugą liczbę");
+    number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wynik to");
+    Console.WriteLine(number1 * number2);
+}
+else if (operation == 4)
+{
+    Console.WriteLine("Wybierz na klawiaturze pierwszą liczbę");
+    number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wybierz na klawiaturze drugą liczbę");
+    number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Wynik to to");
+    Console.WriteLine(number1 / number2);
+}
+else if (operation == 5)
+{
+    Console.WriteLine("Witaj w operacjach specjalnych");
+    Console.WriteLine("1. Silnia");
+    Console.WriteLine("2. Sprawdzenie czy liczba jest doskonała");
+    Console.WriteLine("3. Potęgowanie");
+    Console.WriteLine("4. Średnia z liczb");
+    Console.WriteLine("5. Największa i najmniejsza liczba");
+    int op2 = Convert.ToInt32(Console.ReadLine());
+    if (op2 == 1)
+    {
+        int num; int y = 1;
+        Console.WriteLine("Podaj liczbę (różną od zera) z której obliczę silnie");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num == 1)
+        {
+            Console.WriteLine(" 1! = 1");
+                }
+        else
+        {
+            while (num != 1)
+            {
+                y = y * num;
+                num = num - 1;
+            }
+            Console.WriteLine(y);
+        }
+       
+        
+    
+    }
+    if (op2 == 2)
+            {
+        Console.WriteLine ("Podaj liczbę a ja sprawdzę czy jest doskonała");
+    }
+}
